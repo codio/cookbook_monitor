e9cfa865818cefdb2a19a47ca39a0e39f9b2a346
Joel Moss <joel@developwithstyle.com>
2013-12-02 15:15:06 +0000

    Added more system checks

--- recipes/system.rb
+++ recipes/system.rb
@@ -2,22 +2,25 @@
 # Cookbook Name:: monitor
 # Recipe:: system
 #
-# Adds system level checks.
+# Checks load, CPU, Disk and RAM, and sends notifications via mail.
 #
 
+%w( load cpu disk ram swap ).each do |type|
 
-#
-# Check load and send notifications via mail.
-#
+  remote_file "/etc/sensu/plugins/check-#{type}.rb" do
+    if type == 'swap'
+      source "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/check-#{type}.sh"
+    else
+      source "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/check-#{type}.rb"
+    end
+    mode 0755
+  end
 
-remote_file '/etc/sensu/plugins/check-load.rb' do
-  source 'https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/check-load.rb'
-  mode 0755
-end
+  sensu_check "system_#{type}" do
+    type 'status'
+    command type == 'swap' ? "check-#{type}.sh" : "check-#{type}.rb"
+    handlers [ 'mailer' ]
+    subscribers [ 'all' ]
+  end
 
-sensu_check 'system_load' do
-  type 'status'
-  command 'check-load.rb'
-  handlers [ 'mailer' ]
-  subscribers [ 'all' ]
 end
