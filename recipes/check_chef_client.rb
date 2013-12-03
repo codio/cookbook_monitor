7452c9dfc522099b057958948d0ecbcef38b6887
Joel Moss <joel@developwithstyle.com>
2013-12-03 08:06:14 +0000

    Removed chef-client process check

--- recipes/check_chef_client.rb
+++ recipes/check_chef_client.rb
@@ -5,14 +5,18 @@
 # Checks that the chef-client process is up and running.
 #
 
-remote_file "/etc/sensu/plugins/check-procs.rb" do
-  source "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/processes/check-procs.rb"
-  mode 0755
-end
+# remote_file "/etc/sensu/plugins/check-procs.rb" do
+#   source "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/processes/check-procs.rb"
+#   mode 0755
+# end
+
+# sensu_check "processes_chef_client" do
+#   type 'status'
+#   command "check-procs.rb -p chef-client -W 1"
+#   handlers [ 'mailer' ]
+#   subscribers [ 'all' ]
+# end
 
 sensu_check "processes_chef_client" do
-  type 'status'
-  command "check-procs.rb -p chef-client -W 1"
-  handlers [ 'mailer' ]
-  subscribers [ 'all' ]
+  action :delete
 end
