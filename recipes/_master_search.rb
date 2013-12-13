c211fb8cc046900efccfc8d14b88021f817dd046
Joel Moss <joel@developwithstyle.com>
2013-12-13 13:40:49 +0000

    Fixed issue where a master node could not be found

--- recipes/_master_search.rb
+++ recipes/_master_search.rb
@@ -22,24 +22,26 @@ ip_type = node["monitor"]["use_local_ipv4"] ? "local_ipv4" : "public_ipv4"
 master_address = node["monitor"]["master_address"]
 
 case
-when Chef::Config[:solo]
+when Chef::Config[:solo] || Chef::Config[:zero]
   master_address ||= "localhost"
 when master_address.nil?
   if node["recipes"].include?("monitor::master")
     master_address = "localhost"
   else
-    master_node = case
-    when node["monitor"]["environment_aware_search"]
+    master_node = if node["monitor"]["environment_aware_search"]
       search(:node, "chef_environment:#{node.chef_environment} AND recipes:monitor\\:\\:master").first
     else
       search(:node, "recipes:monitor\\:\\:master").first
     end
 
-    master_address = case
-    when master_node.has_key?("cloud")
-      master_node["cloud"][ip_type] || master_node["ipaddress"]
+    if master_node.nil?
+      master_address = "localhost"
     else
-      master_node["ipaddress"]
+      master_address = if master_node.has_key?("cloud")
+        master_node["cloud"][ip_type] || master_node["ipaddress"]
+      else
+        master_node["ipaddress"]
+      end
     end
   end
 end
