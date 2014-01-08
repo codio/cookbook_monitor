41972ccc151984a6e6c4eef8269c93c1c7d1eaee
Joel Moss <joel@developwithstyle.com>
2014-01-08 23:16:03 +0000

    Bumped version to 0.2.4
    - check_system recipe now checks that URL exists when fetching the plugin from Github.
    - Bumped sensu cookbook dep to >= 0.8
    - Removed twiliosms handler as it ain't working anyway
    - Increased occurrences to 3

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email "portertech@gmail.com"
 license          "Apache 2.0"
 description      "A cookbook for monitoring services, using Sensu, a monitoring framework."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.2.4"
 
 supports 'ubuntu'
 
 depends 'apt'
-depends 'sensu'
+depends 'sensu', '>= 0.8'
 depends 'sudo'
 depends 'python'
 depends 'logrotate'
--- recipes/check_system.rb
+++ recipes/check_system.rb
@@ -7,17 +7,19 @@
 
 %w( load cpu disk ram ).each do |type|
 
+  url = "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/check-#{type}.rb"
   remote_file "/etc/sensu/plugins/check-#{type}.rb" do
-    source "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/check-#{type}.rb"
+    source url
     mode 0755
+    only_if "curl -s -o /dev/null -w \"%{http_code}\" #{url} | grep 200"
   end
 
   sensu_check "system_#{type}" do
     type 'status'
     command "check-#{type}.rb"
-    handlers %w( mailer twiliosms )
+    handlers %w( mailer )
     subscribers [ 'all' ]
-    additional occurrences: 2
+    additional occurrences: 3
   end
 
 end
