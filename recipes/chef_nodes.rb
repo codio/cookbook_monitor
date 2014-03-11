deffce318bcd81880c19dd6929ba84039db4b0eb
Joel Moss <joel@developwithstyle.com>
2014-03-11 13:58:33 +0000

    Reverted last

--- recipes/chef_nodes.rb
+++ recipes/chef_nodes.rb
@@ -3,16 +3,23 @@
 # Recipe:: chef_nodes
 #
 
-url = "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/chef/check-chef-nodes.rb"
-remote_file "/etc/sensu/plugins/check-chef-nodes.rb" do
-  source url
-  mode 0755
-  only_if "curl -s -o /dev/null -w \"%{http_code}\" #{url} | grep 200"
-end
+# sensu_gem "chef"
+
+# url = "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/chef/check-chef-nodes.rb"
+# remote_file "/etc/sensu/plugins/check-chef-nodes.rb" do
+#   source url
+#   mode 0755
+#   only_if "curl -s -o /dev/null -w \"%{http_code}\" #{url} | grep 200"
+# end
+
+# sensu_check "chef_nodes" do
+#   type 'status'
+#   command "check-chef-nodes.rb -U #{Chef::Config[:chef_server_url]} -C #{Chef::Config[:node_name]} -K #{Chef::Config[:client_key]}"
+#   handlers [ 'default' ]
+#   subscribers [ 'all' ]
+# end
+
 
 sensu_check "chef_nodes" do
-  type 'status'
-  command "check-chef-nodes.rb -U #{Chef::Config[:chef_server_url]} -C #{Chef::Config[:node_name]} -K #{Chef::Config[:client_key]}"
-  handlers [ 'default' ]
-  subscribers [ 'all' ]
+  action :delete
 end
